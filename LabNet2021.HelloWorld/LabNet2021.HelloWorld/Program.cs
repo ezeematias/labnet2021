6fc832f1c45847d0b8f2a7aa3eefe967f8e18fc3
ezeematias <eunia69@gmai.com>
2021-09-06 22:51:21 -0300

    Se sube código HelloWorld
    Se sube código junto con los archivos de proyecto y solución de HelloWorid.

---
+++ LabNet2021.HelloWorld/LabNet2021.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabNet2021.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
