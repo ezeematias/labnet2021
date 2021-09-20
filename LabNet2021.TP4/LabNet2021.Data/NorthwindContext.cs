019ce32c8e9fe05ad8b8bc3ef11ce3b24570ec93
Ezequiel Matías Unía <pequenio.casanova@hotmail.com>
2021-09-20 14:28:14 -0300

    Primer commit del TP4
    Se crean las bibliotecas de clase.
    Se Configura el EF.

---
+++ LabNet2021.TP4/LabNet2021.Data/NorthwindContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LabNet2021.Data
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+            : base("name=NorthwindConnection")
+        {
+        }
+
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<Suppliers> Suppliers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Employees>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employees2)
+                .HasForeignKey(e => e.ReportsTo);
+        }
+    }
+}
---
+++ LabNet2021.TP4/LabNet2021.Data/Suppliers.cs
@@ -0,0 +1,48 @@
+namespace LabNet2021.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Suppliers
+    {
+        [Key]
+        public int SupplierID { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CompanyName { get; set; }
+
+        [StringLength(30)]
+        public string ContactName { get; set; }
+
+        [StringLength(30)]
+        public string ContactTitle { get; set; }
+
+        [StringLength(60)]
+        public string Address { get; set; }
+
+        [StringLength(15)]
+        public string City { get; set; }
+
+        [StringLength(15)]
+        public string Region { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(15)]
+        public string Country { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [StringLength(24)]
+        public string Fax { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string HomePage { get; set; }
+    }
+}
